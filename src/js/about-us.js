be079d7f9f443aca47844f090a8d384db8a3fd1a
Yana Bondarenko <yana-lyana27@mail.ru>
2026-03-04 15:05:12 +0100

    add javascript,library Swiper to section about-us

--- src/js/about-us.js
+++ src/js/about-us.js
@@ -1,7 +1,11 @@
  import Swiper from 'swiper';
-import 'swiper/css/keyboard';
+ import { Navigation, Pagination, Keyboard } from 'swiper/modules';
+ import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
     const swiper = new Swiper(".swiper", {
+      modules: [Navigation, Pagination, Keyboard],
       slidesPerView: 1,
       spaceBetween: 30,
       keyboard: {
@@ -10,11 +14,16 @@ import 'swiper/css/keyboard';
       pagination: {
         el: ".swiper-pagination",
         clickable: true,
+        type: 'bullets',
+        bulletActiveClass:"swiper-pagination-bullet-active",
+        dynamicBullets: true,
       },
       navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
+        addIcons: false,
+        disabledClass: "swiper-btn-disabled"
       },
     });
-const mySwiper = document.querySelector('.swiper').swiper;
-swiper.slideNext();
+/* const mySwiper = document.querySelector('.swiper').swiper;
+swiper.slideNext(); */
