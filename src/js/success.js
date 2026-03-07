4753370320f16f76732cf4be607f9ab7da988c16
Artem <stuzukartem12@gmail.com>
2026-03-07 21:08:30 +0200

    fix all success

--- src/js/success.js
+++ src/js/success.js
@@ -1,34 +1,28 @@
 import Swiper from 'swiper';
 import { Navigation, Pagination, Keyboard } from 'swiper/modules';
-
 import axios from 'axios';
 import 'css-star-rating/css/star-rating.css';
-
-const swiperMarkup = document.querySelector('.swiper-wrapper');
-
-export function createReviews(reviews) {
+const swiperMarkup = document.querySelector('.swiper-reviews .swiper-wrapper');
+function createReviews(reviews) {
   const markup = reviews
     .map(
       ({ rate, description, author }) => `
         <div class="swiper-slide">
-        <div class="review-card-slide">
-        <div class="rating star-icon value-${Math.floor(rate)} ${rate % 1 === 0.5 ? 'half' : ''}">
-      <div class="star-container">
-        ${'<div class="star"><svg class="star-empty"><use xlink:href="#star-empty"></use></svg><svg class="star-half"><use xlink:href="#star-half"></use></svg><svg class="star-filled"><use xlink:href="#star-filled"></use></svg></div>'.repeat(5)}
-      </div>
-    </div>
-        <p class="review-card-text">${description}</p>
-        <span class="review-author">${author}</span>
-        </div>
+          <div class="review-card-slide">
+            <div class="rating star-icon value-${Math.floor(rate)} ${rate % 1 === 0.5 ? 'half' : ''}">
+              <div class="star-container">
+                ${'<div class="star"><svg class="star-empty"><use href="/img/icons.svg#star-empty"></use></svg><svg class="star-half"><use href="/img/icons.svg#star-half"></use></svg><svg class="star-filled"><use href="/img/icons.svg#star-filled"></use></svg></div>'.repeat(5)}
+              </div>
+            </div>
+            <p class="review-card-text">${description}</p>
+            <span class="review-author">${author}</span>
+          </div>
         </div>`
     )
     .join('');
-
   swiperMarkup.insertAdjacentHTML('beforeend', markup);
 }
-
 axios.defaults.baseURL = 'https://paw-hut.b.goit.study/';
-
 async function fetchReviews() {
   const { data } = await axios.get('/api/feedbacks');
   return data.feedbacks;
@@ -36,8 +30,7 @@ async function fetchReviews() {
 try {
   const reviews = await fetchReviews();
   createReviews(reviews);
-
-  const swiper = new Swiper('.swiper', {
+  const swiperReviews = new Swiper('.swiper-reviews', {
     modules: [Navigation, Pagination, Keyboard],
     slidesPerView: 1,
     spaceBetween: 30,
@@ -45,15 +38,15 @@ try {
       enabled: true,
     },
     pagination: {
-      el: '.swiper-pagination',
+      el: '.swiper-pagination-reviews',
       clickable: true,
       type: 'bullets',
       bulletActiveClass: 'swiper-pagination-bullet-active',
       dynamicBullets: true,
     },
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-nav-buttons .swiper-button-next',
+      prevEl: '.swiper-nav-buttons .swiper-button-prev',
       addIcons: false,
       disabledClass: 'swiper-btn-disabled',
     },
@@ -65,6 +58,5 @@ try {
     },
   });
 } catch (error) {
-  console.error('API Error (createOrder):', error);
-  throw error;
+  console.error('API Error:', error);
 }
